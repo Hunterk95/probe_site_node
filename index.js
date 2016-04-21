f8b856cb25e851934e70f84fc1084faa6b5685bf
Hunterk95 <hunterk95@mail.ru>
2016-04-21 15:54:50 +0300

    для начала

---
+++ index.js
@@ -0,0 +1,11 @@
+var server = require("./server");
+var router = require("./router");
+var request_handlers = require("./request_handlers");
+
+var handle = {}
+handle["/"] = request_handlers.start;
+handle["/start"] = request_handlers.start;
+handle["/upload"] = request_handlers.upload;
+handle["/show"] = request_handlers.show;
+
+server.start(router.route, handle);
---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var url = require("url");
+
+var port = 8888;
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
+  }
+http.createServer(onRequest).listen(port);
+console.log("server run on port ", port);
+}
+
+exports.start = start;
